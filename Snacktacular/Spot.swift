83e220c8538a77f44d3c0ca9c43458281a20b818
Kathryn Tatum <tatumka@bc.edu>
2020-04-12 18:59:22 -0400

    9.5 Saving Data to the Cloud

--- Snacktacular/Spot.swift
+++ Snacktacular/Spot.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreLocation
+import Firebase
 
 class Spot {
     var name: String
@@ -17,6 +18,18 @@ class Spot {
     var numberOfReviews: Int
     var postingUserID: String
     var documentID: String
+    
+    var longitude: CLLocationDegrees {
+        return coordinate.longitude
+    }
+    
+    var latitude: CLLocationDegrees {
+        return coordinate.latitude
+    }
+    
+    var dictionary: [String: Any] {
+        return ["name": name, "address": address, "longitude": longitude, "latitude": latitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
+    }
     
     init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
         self.name = name
@@ -31,4 +44,39 @@ class Spot {
     convenience init() {
         self.init(name: "", address: "", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
     }
+    func saveData(completion: @escaping (Bool) -> ()) {
+        let db = Firestore.firestore()
+        //Grab UserID
+        guard let postingUserID = (Auth.auth().currentUser?.uid) else {
+            print("*** ERROR: Could not save data because we don't have a valid postingUserID")
+            return completion(false)
+        }
+        self.postingUserID = postingUserID
+        //Create dictionary representing data we want to save
+        let dataToSave = self.dictionary
+        if self.documentID != "" {
+            let ref = db.collection("spots").document(self.documentID)
+            ref.setData(dataToSave) { (error) in
+                if let error = error {
+                    print("*** ERROR updating document \(self.documentID) \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    print("^^^ Document updated with \(ref.documentID)")
+                    completion(true)
+                }
+            }
+        } else {
+            var ref: DocumentReference? = nil
+            ref = db.collection("spots").addDocument(data: dataToSave) { error in
+                if let error = error {
+                    print("*** ERROR creating new document \(error.localizedDescription)")
+                    completion(false)
+                } else {
+                    print("^^^ new document created with ref ID \(ref?.documentID ?? "unknown")")
+                    completion(true)
+                }
+            }
+        }
+    }
+    
 }
--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -33,6 +33,15 @@ class SpotDetailViewController: UIViewController {
         nameField.text = spot.name
         addressField.text = spot.address
     }
+    
+    func leaveViewController() {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
 
     @IBAction func photoButtonPressed(_ sender: UIButton) {
     }
@@ -41,6 +50,13 @@ class SpotDetailViewController: UIViewController {
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        spot.saveData { success in
+            if success {
+                self.leaveViewController()
+            } else {
+                print("*** ERROR: Couldn't leave this view controller because data wasn't saved")
+            }
+        }
     }
     
     @IBAction func lookupPlacePressed(_ sender: UIBarButtonItem) {
@@ -52,12 +68,7 @@ class SpotDetailViewController: UIViewController {
     
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
-        let isPresentingInAddMode = presentingViewController is UINavigationController
-        if isPresentingInAddMode {
-            dismiss(animated: true, completion: nil)
-        } else {
-            navigationController?.popViewController(animated: true)
-        }
+        leaveViewController()
     }
 }
 
